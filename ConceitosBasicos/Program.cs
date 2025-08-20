9c15a03190fb3890362032cbb315410539f194cd
Elyenai <elyenai.cassiano.medeiros@hotmail.com>
2025-08-20 19:48:43 -0300

    Limpeza do código

--- ConceitosBasicos/Program.cs
+++ ConceitosBasicos/Program.cs
@@ -21,29 +21,14 @@ salario = Convert.ToDecimal(Console.ReadLine());
 decimal salarioComDesconto = 0m;
 
 if (salario < 3036m)
-{
     salarioComDesconto = salario;
-    Console.WriteLine($"Seu salário não terá desconto do IRPF e continuará: {salario}");
-}
-else { 
-if (salario >= 3036m && salario < 3533.30m)
-{
+else if (salario >= 3036m && salario < 3533.30m)
     salarioComDesconto = salario - ((salario * 0.075m) - 182.16m);
-}
-
-if (salario >= 3533.31m && salario < 4688.85m) 
-{
+else if (salario >= 3533.31m && salario < 4688.85m)
     salarioComDesconto = salario - ((salario * 0.15m) - 394.16m);
-        }
-
-if (salario >= 4688.85m && salario < 5830.85m)
-{
+else if (salario >= 4688.85m && salario < 5830.85m)
     salarioComDesconto = salario - ((salario * 0.225m) - 675.49m);
-        }
-
-if (salario >= 5830.85m)
-{
+else if (salario >= 5830.85m)
     salarioComDesconto = salario - ((salario * 0.275m) - 908.73m);
-}
 
-Console.WriteLine($"O seu salário após o desconto do IRPF será: {salarioComDesconto}");}
+Console.WriteLine($"O seu salário após o desconto do IRPF será: {salarioComDesconto}");
